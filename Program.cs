967e7da4648a2f8a326cfbe7a568ad8b5290ce5c
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-09-01 15:47:45 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Calcy;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 18;
+            double b = 3;
+            
+            Addition ad = new Addition();
+            Substraction sub = new Substraction();
+            Multiplication mul = new Multiplication();
+            Division div = new Division();
+            Qual eq = new Qual();
+            Greater gr = new Greater();
+            
+            ad.Add(a,b);
+            sub.subs(a, b);
+            mul.mult(a, b);
+            div.Divi(a, b);
+            eq.eqa(a, b);
+            gr.great(a, b);
+        }
+    }
+}
